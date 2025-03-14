49056e74d69bb9b342eb9bcc30d5a78d39594283
temirgaleevroman <roman.temirgaleev@gmail.com>
2025-03-14 17:21:53 +0300

    create project-work

--- conftest.py
+++ conftest.py
@@ -1,24 +1,56 @@
-import allure
 import pytest
-import os
 from selenium import webdriver
-from datetime import datetime
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def pytest_addoption(parser):
+    """Добавление опций командной строки для pytest."""
+    parser.addoption("--browser", action="store", default="chrome",
+                     help="Choose browser: chrome, firefox")
+    parser.addoption("--executor", action="store", default="localhost")
+    parser.addoption("--bv")
+
 
 
 @pytest.fixture()
-def driver():
-    # Настройка драйвера
-    options = webdriver.ChromeOptions()
+def browser(request):
+    """Фикстура для инициализации веб-драйвера на основе аргументов."""
+    browser_name = request.config.getoption("--browser")
+    executor = request.config.getoption("--executor")
+    version = request.config.getoption("--bv")
 
-    # Создание экземпляра драйвера
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    executor_url = f"http://{executor}:4444/wd/hub"
 
-    # Перед каждым тестом выполняется установка драйвера
-    yield driver
+    # Инициализация драйвера на основе выбора браузера
+    if browser_name == "chrome":
+        options = ChromeOptions()
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+    else:
+        raise ValueError(f"Unsupported browser: {browser_name}")
 
-    # После выполнения теста драйвер закрывается
-    driver.quit()
+    caps = {
+        "browserName": browser_name,
+        "browserVersion": version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    for k, v in caps.items():
+        options.set_capability(k, v)
+
+    browser = webdriver.Remote(
+        command_executor=executor_url,
+        options=options
+    )
+
+    browser.maximize_window()
+
+    yield browser
+
+    browser.quit()
 
 
