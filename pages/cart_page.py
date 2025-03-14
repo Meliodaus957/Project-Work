7e0dddc28b511cbbc6cfe5e076c6446eb90836d3
temirgaleevroman <roman.temirgaleev@gmail.com>
2025-03-14 15:16:10 +0300

    create project-work

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -6,7 +6,7 @@ class CartPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def wait_for_element(self, *locator, timeout=10):
+    def wait_for_element(self, locator, timeout=10):
         WebDriverWait(self.driver, timeout).until(
             EC.visibility_of_element_located(locator)
         )
