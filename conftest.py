540b850abddaa95aa701366712255f3249ef46d9
temirgaleevroman <roman.temirgaleev@gmail.com>
2025-03-14 16:51:41 +0300

    create project-work

--- conftest.py
+++ conftest.py
@@ -2,58 +2,27 @@ import allure
 import pytest
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from datetime import datetime
-
-def pytest_addoption(parser):
-    """Добавление опций командной строки для pytest."""
-    parser.addoption("--browser", action="store", default="chrome",
-                     help="Choose browser: chrome, firefox")
-    parser.addoption("--executor", action="store", default="selenoid")
-    parser.addoption("--bv")
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
-def driver(request):
-    """Фикстура для инициализации веб-драйвера на основе аргументов."""
-    browser_name = request.config.getoption("--browser")
-    executor = request.config.getoption("--executor")
-    version = request.config.getoption("--bv")
+def driver():
+    # Настройка драйвера
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Запуск браузера в фоновом режиме (без графического интерфейса)
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    executor_url = f"http://{executor}:4444/wd/hub"
+    # Создание экземпляра драйвера
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    # Инициализация драйвера на основе выбора браузера
-    if browser_name == "chrome":
-        options = ChromeOptions()
-    elif browser_name == "firefox":
-        options = FirefoxOptions()
-    else:
-        raise ValueError(f"Unsupported browser: {browser_name}")
+    # Перед каждым тестом выполняется установка драйвера
+    yield driver
 
-    caps = {
-        "browserName": browser_name,
-        "browserVersion": version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
-        }
-    }
-
-    for k, v in caps.items():
-        options.set_capability(k, v)
-
-    browser = webdriver.Remote(
-        command_executor=executor_url,
-        options=options
-    )
-
-    browser.maximize_window()
-
-
-    yield browser
-
-    browser.quit()
+    # После выполнения теста драйвер закрывается
+    driver.quit()
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
